15d2765cfc28aff6d967b8947187e6ae5c136c46
Edouard Topin <ed.topin@gmail.com>
2017-08-29 19:39:38 +0100

    lint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,10 +11,9 @@ export class AppComponent implements OnInit {
   public age: number;
   constructor() {
       this.birthdate = new Date(1993, 12, 2);
-      var timeDiff = Math.abs(Date.now() - this.birthdate);
-      //Used Math.floor instead of Math.ceil
-      //so 26 years and 140 days would be considered as 26, not 27.
-      this.age = Math.floor((timeDiff / (1000 * 3600 * 24))/365);
+      // Used Math.floor instead of Math.ceil
+      // so 26 years and 140 days would be considered as 26, not 27.
+      this.age = Math.floor(( Math.abs(Date.now() - this.birthdate) / (1000 * 3600 * 24))/365);
     }
 
  ngOnInit() { }
